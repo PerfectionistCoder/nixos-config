21ac2695506d7f7fdd8cf971a29cdbab6258a7df
PerfectionistCoder <github.hug438@outlook.com>
2024-10-15 21:26:58 +0100

    firefox privacy security update

--- home-manager/features/firefox/search.nix
+++ home-manager/features/firefox/search.nix
@@ -5,17 +5,17 @@
     "Brave" = {
       urls = [
         { template = "https://search.brave.com/search?q={searchTerms}"; }
-        {
-          template = "https://search.brave.com/api/suggest?q={searchTerms}";
-          type = "application/x-suggestions+json";
-        }
+        # {
+        #   template = "https://search.brave.com/api/suggest?q={searchTerms}";
+        #   type = "application/x-suggestions+json";
+        # }
       ];
       iconUpdateURL = "https://brave.com/static-assets/images/brave-favicon.png";
       updateInterval = 24 * 60 * 60 * 1000;
       definedAliases = [ "@brave" ];
     };
 
-    # "Google".metaData.hidden = true;
+    "Google".metaData.hidden = true;
     "eBay".metaData.hidden = true;
     "Wikipedia (en)".metaData.hidden = true;
     "DuckDuckGo".metaData.hidden = true;

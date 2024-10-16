1d59e27ed3f5ddd58b6a7127bc2bfe49ffa5a8fd
PerfectionistCoder <github.hug438@outlook.com>
2024-10-16 20:27:33 +0100

    zsh alias fix

--- home-manager/features/zsh/aliases.nix
+++ home-manager/features/zsh/aliases.nix
@@ -3,7 +3,7 @@
 
   to-nixos = "cd $NIX_CONFIG_HOME";
   nix-fmt = "(to-nixos; nix fmt)";
-  nix-clean = "nix-env --delete-generation old; sudo nix-collect-garbage -d";
+  nix-clean = "nix-env --delete-generations old; sudo nix-collect-garbage -d";
   hm-clean = "home-manager expire-generations $(date +%X)";
   nix-clean-boot-entries = "sudo /run/current-system/bin/switch-to-configuration boot";
   nix-update = "sudo nix flake update $NIX_CONFIG_HOME";

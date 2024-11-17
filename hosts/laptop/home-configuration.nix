11cbb6a9fc375edddbc0efcec04211800f3bae0a
PerfectionistCoder <github.hug438@outlook.com>
2024-11-17 22:38:17 +0000

    new apps

--- hosts/laptop/home-configuration.nix
+++ hosts/laptop/home-configuration.nix
@@ -4,6 +4,14 @@
   home.packages = with pkgs; [
     gnome.file-roller
     microsoft-edge
+    rclone
+    keepassxc
+    libreoffice
+    hunspell
+    hunspellDicts.en_GB-ize
+    evince
+    nixfmt-rfc-style
+    ungoogled-chromium
   ];
 
   custom.defaultShellCommand = "zsh";

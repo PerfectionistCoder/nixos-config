5f0a772403aabc05ed97dfae2386eccba7715b92
PerfectionistCoder <github.hug438@outlook.com>
2024-11-21 23:02:34 +0000

    remove git global user

--- home-manager/self/git.nix
+++ home-manager/self/git.nix
@@ -2,8 +2,8 @@
 {
   programs.git = {
     enable = true;
-    userName = "3b4d80a2-0c72-4839-8000-c72d4daf561f";
-    userEmail = "3b4d80a2-0c72-4839-8000-c72d4daf561f";
+    userName = "";
+    userEmail = "";
     extraConfig = {
       init.defaultBranch = "main";
     };

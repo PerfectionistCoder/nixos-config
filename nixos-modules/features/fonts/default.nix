e5444be955a4fad8261482591973922c741c13be
PerfectionistCoder <github.hug438@outlook.com>
2024-12-20 19:13:13 +0000

    fix font

--- nixos-modules/features/fonts/default.nix
+++ nixos-modules/features/fonts/default.nix
@@ -4,15 +4,12 @@
     fontDir.enable = true;
     packages = with pkgs; [
       cascadia-code
-      quicksand
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
     ];
     fontconfig = {
       defaultFonts = {
         sansSerif = [
-          "Quicksand"
           "Noto Sans CJK HK"
         ];
         serif = [ "Noto Serif CJK HK" ];

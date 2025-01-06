3f919e3673a75bbab4d1bb9b1716fd897ba6f965
PerfectionistCoder <github.hug438@outlook.com>
2025-01-06 11:57:34 +0000

    remove firefox bookmarks' keyword

--- home-manager/features/firefox/bookmarks.nix
+++ home-manager/features/firefox/bookmarks.nix
@@ -8,17 +8,14 @@
         bookmarks = [
           {
             name = "NixOS Search";
-            keyword = "pkgs";
             url = "https://search.nixos.org/packages";
           }
           {
             name = "Home Manager Options";
-            keyword = "hm";
             url = "https://home-manager-options.extranix.com";
           }
           {
             name = "Noogle";
-            keyword = "noogle";
             url = "https://noogle.dev/";
           }
         ];
@@ -28,30 +25,29 @@
         bookmarks = [
           {
             name = "Youtube Music";
-            keyword = "music";
             url = "https://music.youtube.com/";
           }
           {
             name = "Youtube";
-            keyword = "youtube";
             url = "https://www.youtube.com";
           }
         ];
       }
       {
         name = "Hyprland Wiki";
-        keyword = "hyprland";
         url = "https://wiki.hyprland.org/";
       }
       {
         name = "ChatGPT";
-        keyword = "gpt";
         url = "https://chatgpt.com/";
       }
       {
         name = "Firefox releases";
-        keyword = "update";
         url = "https://www.mozilla.org/en-US/firefox/releases/";
+      }
+      {
+        name = "White board";
+        url = "https://excalidraw.com/";
       }
     ];
   }

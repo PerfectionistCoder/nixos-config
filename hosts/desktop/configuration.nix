ac655904be447025e6f89759e93b2f9ff0c557ba
PerfectionistCoder <github.hug438@outlook.com>
2025-01-07 22:26:23 +0000

    modulize containers

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,8 +1,6 @@
 {
-  config,
-  username,
   pkgs,
-  home-manager,
+  username,
   ...
 }:
 {
@@ -25,86 +23,4 @@
     enable = true;
     extraPackages = with pkgs; [ amdvlk ];
   };
-
-  containers =
-    let
-      hostCfg = config;
-      userName = "browser-user";
-      userUid = hostCfg.users.users."${username}".uid;
-    in
-    {
-      browser = {
-        ephemeral = true;
-        bindMounts = {
-          waylandDisplay = rec {
-            hostPath = "/run/user/${toString userUid}";
-            mountPoint = hostPath;
-          };
-        };
-
-        specialArgs = { inherit home-manager; };
-        config =
-          {
-            home-manager,
-            pkgs,
-            ...
-          }:
-          {
-            imports = [ home-manager.nixosModules.home-manager ];
-
-            system.stateVersion = hostCfg.system.stateVersion;
-
-            hardware.graphics = {
-              enable = true;
-              extraPackages = hostCfg.hardware.graphics.extraPackages;
-            };
-
-            services.getty.autologinUser = userName;
-            users = {
-              mutableUsers = false;
-              users = {
-                root.password = "";
-                "${userName}" = {
-                  uid = userUid;
-                  password = "";
-                  isNormalUser = true;
-                  home = "/home";
-                };
-              };
-            };
-
-            environment.sessionVariables = {
-              WAYLAND_DISPLAY = "wayland-1";
-              MOZ_ENABLE_WAYLAND = "1";
-              XDG_RUNTIME_DIR = "/run/user/${toString userUid}";
-              DISPLAY = ":0";
-            };
-
-            home-manager = {
-              useGlobalPkgs = true;
-              users."${userName}" = {
-                home.stateVersion = hostCfg.system.stateVersion;
-
-                programs.firefox = {
-                  enable = true;
-                };
-              };
-            };
-
-            systemd.services.fix-run-permission = {
-              script = ''
-                #!${pkgs.stdenv.shell}
-                set -euo pipefail
-
-                chown ${userName} /run/user/${toString userUid}
-                chmod u=rwx /run/user/${toString userUid}
-              '';
-              wantedBy = [ "multi-user.target" ];
-              serviceConfig = {
-                Type = "oneshot";
-              };
-            };
-          };
-      };
-    };
 }
---
+++ nixos-modules/containers/default.nix
@@ -0,0 +1,94 @@
+{
+  pkgs,
+  config,
+  lib,
+  username,
+  customLib,
+  home-manager,
+  ...
+}:
+with lib;
+let
+  hostCfg = config;
+  userUid = hostCfg.users.users."${username}".uid;
+
+  traits = username: {
+    base = {
+      config =
+        { ... }:
+        {
+          nixpkgs.pkgs = mkForce pkgs;
+          system.stateVersion = hostCfg.system.stateVersion;
+        };
+    };
+    defaultUser = {
+      config =
+        { ... }:
+        {
+          services.getty.autologinUser = username;
+          users = {
+            mutableUsers = false;
+            users = {
+              root.password = "";
+              "${username}" = {
+                uid = userUid;
+                password = "";
+                isNormalUser = true;
+                home = "/home";
+              };
+            };
+          };
+        };
+    };
+    homeManager = {
+      config =
+        { ... }:
+        {
+          imports = [ home-manager.nixosModules.home-manager ];
+
+          home-manager = {
+            useGlobalPkgs = true;
+            users."${username}" = {
+              home.stateVersion = hostCfg.system.stateVersion;
+            };
+          };
+        };
+    };
+    wayland = {
+      bindMounts = {
+        waylandDisplay = rec {
+          hostPath = "/run/user/${toString userUid}";
+          mountPoint = hostPath;
+        };
+      };
+      ephemeral = true;
+
+      config =
+        { ... }:
+        {
+          environment.sessionVariables = {
+            WAYLAND_DISPLAY = "wayland-1";
+            MOZ_ENABLE_WAYLAND = "1";
+            XDG_RUNTIME_DIR = "/run/user/${toString userUid}";
+            DISPLAY = ":0";
+          };
+        };
+    };
+  };
+in
+{
+  containers = listToAttrs (
+    map (
+      template:
+      let
+        name = removeSuffix ".nix" (customLib.getBaseName template);
+        username = name + "-user";
+        attrset = import ./${customLib.getBaseName template} { inherit hostCfg username; };
+      in
+      {
+        inherit name;
+        value = mkMerge ((attrValues (traits username)) ++ [ attrset.container ]);
+      }
+    ) (customLib.getPaths.excludeDefaultAndDirs ./.)
+  );
+}

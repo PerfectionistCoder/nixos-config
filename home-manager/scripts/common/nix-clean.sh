213ee020b611993ec0d635c5a0d11895b402113b
PerfectionistCoder <github.hug438@outlook.com>
2025-01-08 20:08:22 +0000

    update scripts

--- home-manager/scripts/common/nix-clean.sh
+++ home-manager/scripts/common/nix-clean.sh
@@ -1,3 +1,8 @@
-sudo /run/current-system/sw/bin/nix-collect-garbage -d
-nix-env --delete-generations old
-home-manager expire-generations $(date +%X)
+if [[ $1 == "--raw" ]]; then
+  shift
+  sudo /run/current-system/sw/bin/nix-collect-garbage $*
+else
+  sudo /run/current-system/sw/bin/nix-collect-garbage -d
+  nix-env --delete-generations old 2 >/dev/null &>1
+  home-manager expire-generations $(date +%X) 2 >/dev/null &>1
+fi
--- home-manager/scripts/common/nixos-switch.sh
+++ home-manager/scripts/common/nixos-switch.sh
@@ -1,4 +1,8 @@
-FLAKE=$1
-shift
-
-sudo /run/current-system/sw/bin/nixos-rebuild switch --flake $NIXOS_CONFIG_HOME/#${FLAKE:-$FLAKE_HOSTNAME} ${@:---impure}
+if [[ $1 == "--raw" ]]; then
+  shift
+  sudo /run/current-system/sw/bin/nixos-rebuild $*
+else
+  cmd=$1
+  shift
+  sudo /run/current-system/sw/bin/nixos-rebuild ${cmd:-test} --flake $NIXOS_CONFIG_HOME/#$FLAKE_HOSTNAME --impure $*
+fi

4ab83cc637600bf5256630a7fd2eb7d7419934de
PerfectionistCoder <github.hug438@outlook.com>
2025-01-08 12:59:06 +0000

    add fonts to firefox container

--- nixos-modules/containers/firefox.nix
+++ nixos-modules/containers/firefox.nix
@@ -7,7 +7,7 @@
   ];
   container = {
     config =
-      { ... }:
+      { pkgs, ... }:
       {
         hardware.graphics = {
           enable = true;
@@ -15,6 +15,23 @@
         };
 
         services.pipewire.enable = true;
+
+        fonts = {
+          packages = with pkgs; [
+            (ibm-plex.override { families = [ "sans" ]; })
+            noto-fonts-cjk-sans
+            noto-fonts-cjk-serif
+          ];
+          fontconfig = {
+            defaultFonts = {
+              sansSerif = [
+                "IBM Plex Sans"
+                "Noto Sans CJK HK"
+              ];
+              serif = [ "Noto Serif CJK HK" ];
+            };
+          };
+        };
 
         home-manager = {
           users."${username}" = {

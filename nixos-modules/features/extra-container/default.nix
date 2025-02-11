5460d106503fa524f577ffc6cba4cedcc2df7ff4
PerfectionistCoder <github.hug438@outlook.com>
2025-02-11 20:08:31 +0000

    add nat

--- nixos-modules/features/extra-container/default.nix
+++ nixos-modules/features/extra-container/default.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
   programs.extra-container.enable = true;
+
+  networking.nat = {
+    enable = true;
+    internalInterfaces = [
+      "ve-+"
+    ];
+    externalInterface = "wlp4s0";
+    enableIPv6 = true;
+  };
 }

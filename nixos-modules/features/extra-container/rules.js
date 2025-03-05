cee36387575c3d6c59b81ff844799d51bc920313
PerfectionistCoder <github.hug438@outlook.com>
2025-03-05 16:55:26 +0000

    fix polkit rule

--- nixos-modules/features/extra-container/rules.js
+++ nixos-modules/features/extra-container/rules.js
@@ -1,10 +1,10 @@
 polkit.addRule(function (action, subject) {
-    if (
-        ((action.id == 'org.freedesktop.systemd1.manage-units' ||
-            action.id == 'org.freedesktop.systemd1.manage-unit-files')
-            && action.lookup("unit").indexOf("container@") == 0) ||
-        action.id == 'org.freedesktop.machine1.shell'
-
-    )
-        return polkit.Result.YES;
+  if (
+    subject.isInGroup("machine") &&
+    (((action.id == "org.freedesktop.systemd1.manage-units" ||
+      action.id == "org.freedesktop.systemd1.manage-unit-files") &&
+      action.lookup("unit").indexOf("container@") == 0) ||
+      action.id == "org.freedesktop.machine1.shell")
+  )
+    return polkit.Result.YES;
 });
